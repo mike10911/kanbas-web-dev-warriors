90f2d17bc525db71960a80032971d208df7b1704
mike10911 <mikeymundia@gmail.com>
2024-04-20 15:39:49 -0400

    frontend fix

--- src/Kanbas/Courses/Quizzes/Details/index.tsx
+++ src/Kanbas/Courses/Quizzes/Details/index.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import "./index.css";
-import { Link } from 'react-router-dom';
+import "../index.css";
+import { Link } from "react-router-dom";
 import { quizzes } from "../../../Database";
+import { FaEllipsisV } from "react-icons/fa";
 
 interface QuizDetailsProps {
     quizId: string;
@@ -14,10 +16,9 @@ const QuizDetails = () => {
     const quiz = quizzes.find((quiz) => quiz._id === quizId);
     // const API_BASE = process.env.REACT_APP_API_BASE;
 
-
-    // axios 
+    // axios
     // useEffect(() => {
-    //     findCourseQuizzes(courseId)  
+    //     findCourseQuizzes(courseId)
     //         .then((data) => {
     //             setQuizzes(data);
     //             setLoading(false);
@@ -27,85 +28,136 @@ const QuizDetails = () => {
     //             setLoading(false);
     //         });
     // }, [courseId]);
-    
-
-    
-   
-   
 
     if (!quiz) {
         return <div>Loading details...⏳</div>;
     }
 
     return (
-        <div className='text-center'>
+        <div className="text-left">
             <h1>{quiz.title}</h1>
-            <div className='d-flex justify-content-end gap-2'>
-            <Link type="button" to={`Editor/Details`} className="btn btn-primary">EDIT</Link>
-            <Link type="button" to={`Preview`} className="btn btn-primary">PREVIEW</Link>
-            <Link type="button" to = {`...`} className="btn btn-primary">Publish</Link>
+            <div className="d-flex justify-content-end gap-2">
+                <Link type="button" to={`...`} className="btn btn-success">
+                    Publish
+                </Link>
+                <Link
+                    type="button"
+                    to={`Preview`}
+                    className="btn wd-modules-btn p-1"
+                >
+                    Preview
+                </Link>
+                <Link
+                    type="button"
+                    to={`Editor/Details`}
+                    className="btn wd-modules-btn p-1"
+                >
+                    Edit
+                </Link>
+                <Link
+                    type="button"
+                    to={`...`}
+                    className="btn wd-modules-btn p-1"
+                >
+                    <FaEllipsisV />
+                </Link>
             </div>
 
-            <table className='mx-auto'>
+            <table className="mx-auto">
                 <tr>
-                    <td className='list-bold'><b>Quiz Type</b></td>
+                    <td className="list-bold">
+                        <b>Quiz Type</b>
+                    </td>
                     <td>{quiz.type}</td>
                 </tr>
                 <tr>
-                    <td className='list-bold'><b>Points</b></td>
+                    <td className="list-bold">
+                        <b>Points</b>
+                    </td>
                     <td>{quiz.points}</td>
                 </tr>
                 <tr>
-                    <td className='list-bold'><b>Assignment Group</b></td>
+                    <td className="list-bold">
+                        <b>Assignment Group</b>
+                    </td>
                     <td>{quiz.assignmentGroup}</td>
                 </tr>
                 <tr>
-                    <td className='list-bold'><b>Shuffle Answers</b></td>
-                    <td>{quiz.shuffleAnswers ? 'Yes' : 'No'}</td>
+                    <td className="list-bold">
+                        <b>Shuffle Answers</b>
+                    </td>
+                    <td>{quiz.shuffleAnswers ? "Yes" : "No"}</td>
                 </tr>
                 <tr>
-                    <td className='list-bold'><b>Time Limit</b></td>
+                    <td className="list-bold">
+                        <b>Time Limit</b>
+                    </td>
                     <td>{quiz.timeLimit} minutes</td>
                 </tr>
                 <tr>
-                    <td className='list-bold'><b>Multiple Attempts</b></td>
-                    <td>{quiz.multipleAttempts ? 'Yes' : 'No'}</td>
+                    <td className="list-bold">
+                        <b>Multiple Attempts</b>
+                    </td>
+                    <td>{quiz.multipleAttempts ? "Yes" : "No"}</td>
                 </tr>
                 <tr>
-                    <td className='list-bold'><b>Show Correct Answers</b></td>
-                    <td>{new Date(quiz.showCorrectAnswers).toLocaleString()}</td>
+                    <td className="list-bold">
+                        <b>Show Correct Answers</b>
+                    </td>
+                    <td>
+                        {new Date(quiz.showCorrectAnswers).toLocaleString()}
+                    </td>
                 </tr>
                 <tr>
-                    <td className='list-bold'><b>Access Code</b></td>
+                    <td className="list-bold">
+                        <b>Access Code</b>
+                    </td>
                     <td>{quiz.accessCode}</td>
                 </tr>
                 <tr>
-                    <td className='list-bold'><b>One Question at a Time</b></td>
-                    <td>{quiz.oneQuestionAtATime ? 'Yes' : 'No'}</td>
+                    <td className="list-bold">
+                        <b>One Question at a Time</b>
+                    </td>
+                    <td>{quiz.oneQuestionAtATime ? "Yes" : "No"}</td>
                 </tr>
                 <tr>
-                    <td className='list-bold'><b>Webcam Required</b></td>
-                    <td>{quiz.webcamRequired ? 'Yes' : 'No'}</td>
+                    <td className="list-bold">
+                        <b>Webcam Required</b>
+                    </td>
+                    <td>{quiz.webcamRequired ? "Yes" : "No"}</td>
                 </tr>
                 <tr>
-                    <td className='list-bold'><b>Lock Questions After Answering</b></td>
-                    <td>{quiz.lockQuestionsAfterAnswering ? 'Yes' : 'No'}</td>
+                    <td className="list-bold">
+                        <b>Lock Questions After Answering</b>
+                    </td>
+                    <td>{quiz.lockQuestionsAfterAnswering ? "Yes" : "No"}</td>
                 </tr>
             </table>
             <br></br>
-            <table className='mx-auto flex-fill'>
-                <tr className='bordered'>
-                    <td><b>Due</b></td>
-                    <td><b>Available From</b></td>
-                    <td><b>Until</b></td>
+            <table className="mx-auto flex-fill">
+                <tr className="bordered">
+                    <td>
+                        <b>Due</b>
+                    </td>
+                    <td>
+                        <b>Available From</b>
+                    </td>
+                    <td>
+                        <b>Until</b>
+                    </td>
                 </tr>
                 <tr>
-                    <td className='date'>{new Date(quiz.dueDate).toLocaleString()}</td>
-                    <td className='date'>{new Date(quiz.availableDate).toLocaleString()}</td>
-                    <td className='date'>{new Date(quiz.untilDate).toLocaleString()}</td>
+                    <td className="date">
+                        {new Date(quiz.dueDate).toLocaleString()}
+                    </td>
+                    <td className="date">
+                        {new Date(quiz.availableDate).toLocaleString()}
+                    </td>
+                    <td className="date">
+                        {new Date(quiz.untilDate).toLocaleString()}
+                    </td>
                 </tr>
             </table>
-            {/* Render quiz questions and options here */}
         </div>
     );
 };
--- src/Kanbas/Courses/Quizzes/QuizList.tsx
+++ src/Kanbas/Courses/Quizzes/QuizList.tsx
@@ -21,10 +21,10 @@ import { Link } from "react-router-dom";
 import { formatDate } from "../Assignments";
 import axios from "axios";
 // impport quiz data from mock data json file
-import { quizzes } from "../../Database"
+import { quizzes } from "../../Database";
+import { MdOutlineRocketLaunch } from "react-icons/md";
 const API_BASE = process.env.REACT_APP_API_BASE;
 const QUIZ_API = `${API_BASE}/api`;
-
 
 interface Question {
     _id: string;
@@ -58,25 +58,21 @@ interface Quiz {
     untilDate: string;
 }
 
-
 export async function findCourseQuizzes(courseId: string): Promise<Quiz[]> {
-  const response = await axios.get(`${QUIZ_API}/${courseId}/quizzes`);
-  return response.data;
-};
+    const response = await axios.get(`${QUIZ_API}/${courseId}/quizzes`);
+    return response.data;
+}
 export const createQuiz = async (courseId: any, quiz: any) => {
-  const response = await axios.post(`${QUIZ_API}/${courseId}/quizzes`, quiz);
-  return response.data;
+    const response = await axios.post(`${QUIZ_API}/${courseId}/quizzes`, quiz);
+    return response.data;
 };
-
-
 
 function QuizList() {
     // const [quizzes, setQuizzes] = useState<Quiz[]>([]);
     // const [loading, setLoading] = useState<boolean>(true);
     // const [error, setError] = useState<string | null>(null);
     // const { courseId } = useParams();
-
-    
+    // TODO: filter by courseId
 
     // const  handleAddQuiz = () => {
     //     // add quiz
@@ -120,66 +116,81 @@ function QuizList() {
     //     fetchQuizzes();
     // }, [courseId]);
     function getAvailabilityStatus(quiz: Quiz): string {
-      const now = new Date();
-      const availableDate = new Date(quiz.availableDate);
-      const untilDate = new Date(quiz.untilDate);
-      if (now < availableDate) {
-          return "Not available yet";
-      } else if (now > untilDate) {
-          return "Expired";
-      } else {
-          return "Available";
-      }
-  }
-  
+        const now = new Date();
+        const availableDate = new Date(quiz.availableDate);
+        const untilDate = new Date(quiz.untilDate);
+        if (now < availableDate) {
+            return "Not available yet";
+        } else if (now > untilDate) {
+            return "Expired";
+        } else {
+            return "Available";
+        }
+    }
 
     return (
-        <div className='flex-fill' >
+        <div className="flex-fill">
             <h1>Quiz List</h1>
-            <button className="add-quiz-btn">+ Quiz</button>
+            <div className="d-flex justify-content-between">
+                <input
+                    className="border border-muted rounded"
+                    type="text"
+                    placeholder="Search for Quiz"
+                />
+                <div className="d-flex gap-1">
+                    <button className="btn btn-danger d-flex align-items-center">
+                        <FaPlus className="me-1" /> Quiz
+                    </button>
+                    <button className="btn wd-modules-btn p-1">
+                        <FaEllipsisV />
+                    </button>
+                </div>
+            </div>
             <div className="quiz-list-container">
                 {quizzes.length > 0 ? (
                     quizzes.map((quiz, index) => (
-                        <div key={index} className="quiz-box">
-                            <Link
-                                to={`${quiz._id}`}
-                                style={{
-                                    textDecoration: "none",
-                                    display: "block",
-                                    flexGrow: 1,
-                                }}
-                            >
-                                <div>
-                                    <div
-                                        className="quiz-title"
-                                        style={{ fontWeight: "bold" }}
+                        <div className="d-flex flex-row align-items-center justify-content-between quiz-box">
+                            <div className="d-flex flex-row align-items-center gap-3">
+                                <MdOutlineRocketLaunch
+                                    className="fs-5"
+                                    style={{ color: "green" }}
+                                />
+                                <div className="d-flex flex-column">
+                                    <Link
+                                        to={`${quiz._id}`}
+                                        style={{
+                                            textDecoration: "none",
+                                            display: "block",
+                                            flexGrow: 1,
+                                        }}
                                     >
-                                        {quiz.title}
-                                    </div>
+                                        <div
+                                            className="quiz-title"
+                                            style={{ fontWeight: "bold" }}
+                                        >
+                                            {quiz.title}
+                                        </div>
+                                    </Link>
+
                                     <div className="quiz-details">
                                         <span className="availability-status">
-                                {getAvailabilityStatus(quiz)}
-                            </span> |
-                                        Due {formatDate(new Date(quiz.dueDate))} | Points:{" "}
-                                        {quiz.points} | {quiz.questions.length}{" "}
-                                        Questions
+                                            {getAvailabilityStatus(quiz)}
+                                        </span>{" "}
+                                        | Due{" "}
+                                        {formatDate(new Date(quiz.dueDate))} |
+                                        Points: {quiz.points} |{" "}
+                                        {quiz.questions.length} Questions
                                     </div>
                                 </div>
-                            </Link>
-                            <span
-                                className={`publish-status ${
-                                    quiz.isPublished ? "published" : "unpublished"
-                                }`}
-                            >
-                                {quiz.isPublished ?  <FaCheckCircle className='text-success' /> : "🚫"}
-                            </span>
-                            <div
-                                onClick={(e) => e.stopPropagation()}
-                                className="quiz-context-menu"
-                            >
-                                {/* Implement the context menu here */}
-                                ...
                             </div>
+                            <span className="d-flex flex-row gap-1">
+                                {quiz.isPublished ? (
+                                    <FaCheckCircle className="text-success" />
+                                ) : (
+                                    "🚫"
+                                )}
+                                <FaEllipsisV className="ms-2" />
+                            </span>
                         </div>
                     ))
                 ) : (
@@ -193,5 +204,3 @@ function QuizList() {
 }
 
 export default QuizList;
-
-
