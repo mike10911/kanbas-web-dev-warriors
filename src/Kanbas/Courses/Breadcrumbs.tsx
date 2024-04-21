3d2efae8e9f927dc80abd30fc10d09187bb310c9
Alyssa Mui <alyssamui23@gmail.com>
2024-04-21 19:20:19 -0400

    integrate questions editor

--- src/Kanbas/Courses/Breadcrumbs.tsx
+++ src/Kanbas/Courses/Breadcrumbs.tsx
@@ -1,9 +1,10 @@
-import { FaChevronRight, FaGlasses } from 'react-icons/fa';
-import { HiMiniBars3 } from 'react-icons/hi2';
-import { Link, useLocation } from 'react-router-dom';
-import { useMemo } from 'react';
-import './Breadcrumbs.css';
-import { Course } from '..';
+import { FaChevronRight, FaGlasses } from "react-icons/fa";
+import { HiMiniBars3 } from "react-icons/hi2";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useMemo, useState } from "react";
+import "./Breadcrumbs.css";
+import { Course } from "..";
+import * as client from "./Quizzes/Preview/client";
 
 export interface BreadcrumbsProps {
   courseId?: string;
@@ -20,45 +21,57 @@ const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
   const { pathname } = useLocation();
   const page = useMemo(() => {
     const links = [
-      'Home',
-      'Modules',
-      'Piazza',
-      'Grades',
-      'Assignments',
-      'Quizzes',
+      "Home",
+      "Modules",
+      "Piazza",
+      "Grades",
+      "Assignments",
+      "Quizzes",
     ];
-    const routes = pathname.split('/');
+    const routes = pathname.split("/");
     const currentPage = routes.find((route) => links.includes(route));
-    return currentPage === 'Home' ? 'Modules' : currentPage;
+    return currentPage === "Home" ? "Modules" : currentPage;
   }, [pathname]);
 
+  const [quizTitle, setQuizTitle] = useState<string>("");
+  useEffect(() => {
+    const fetchQuizTitle = async (quizId: string) => {
+      const quiz = await client.findQuizById(quizId);
+      setQuizTitle(quiz?.title || "");
+    };
+    if (page === "Quizzes") {
+      const quizId = pathname.split("/Quizzes").at(-1)?.split("/").at(1);
+      quizId && fetchQuizTitle(quizId);
+    }
+  }, [page]);
+
   return (
-    <div className='d-none d-lg-flex justify-content-between align-items-center px-4 py-3'>
-      <div className='d-flex gap-3 align-items-center'>
+    <div className="d-none d-lg-flex justify-content-between align-items-center px-4 py-3">
+      <div className="d-flex gap-3 align-items-center">
         <HiMiniBars3
-          className='me-3 wd-kanbas-hamburger-icon fs-4 icon-btn'
+          className="me-3 wd-kanbas-hamburger-icon fs-4 icon-btn"
           onClick={() => onHamburgerClick((showCourseNav) => !showCourseNav)}
         />
-        <Link className='wd-breadcrumb-header fs-5' to='Home'>
+        <Link className="wd-breadcrumb-header fs-5" to="Home">
           {course?.number} {course?._id} {course?.name}
         </Link>
-        <FaChevronRight className='fs-6' />
-        <p className='m-0 fs-5'>{page}</p>
-        {page === 'Assignments' && pathname.split('/').at(-1) !== page && (
+        <FaChevronRight className="fs-6" />
+        <p className="m-0 fs-5">{page}</p>
+        {page === "Assignments" && pathname.split("/").at(-1) !== page && (
           <>
-            <FaChevronRight className='fs-6' />
-            <p className='m-0 fs-5'>Edit</p>
+            <FaChevronRight className="fs-6" />
+            <p className="m-0 fs-5">Edit</p>
           </>
         )}
-        {page === 'Quizzes' && pathname.split('/').at(-1) !== page && (
+        {page === "Quizzes" && pathname.split("/").at(-1) !== page && (
           <>
-            <FaChevronRight className='fs-6' />
-            <p className='m-0 fs-5'>TODO: update to be quiz title</p>
+            <FaChevronRight className="fs-6" />
+            <p className="m-0 fs-5">{quizTitle}</p>
           </>
         )}
       </div>
-      <button className='btn wd-modules-btn'>
-        <FaGlasses className='me-1' />
+      <button className="btn wd-modules-btn">
+        <FaGlasses className="me-1" />
         Student View
       </button>
     </div>
--- src/Kanbas/Courses/Quizzes/Editor/QuestionsTab.tsx
+++ src/Kanbas/Courses/Quizzes/Editor/QuestionsTab.tsx
@@ -1,27 +1,37 @@
 import { useEffect, useState } from "react";
 import "./QuestionsTab.css";
-import { FaBan, FaCheck, FaPlus, FaTrash } from "react-icons/fa";
+import { FaBan, FaCheck, FaPlus } from "react-icons/fa";
 import { FaMagnifyingGlass } from "react-icons/fa6";
-import { Question, QuestionType } from "../types";
+import { Question } from "../types";
 import QuestionEditor from "./QuestionEditor";
+import EditorNav from "../DetailsEditor/EditorNav";
+import { useParams } from "react-router";
+import * as client from "../Preview/client";
+import { QuestionType } from "../Preview/constants";
+import { Quiz } from "../Preview";
 
 function QuestionsTab() {
-  const [points, setPoints] = useState(0);
-  const [published, setPublished] = useState(false);
-  const [notifyUsers, setNotifyUsers] = useState(false);
+  const { quizId } = useParams();
+  const [quiz, setQuiz] = useState<Quiz | null>(null);
   const [questions, setQuestions] = useState<Question[]>([]);
   const [initialQuestions, setInitialQuestions] = useState<Question[]>([]);
 
-  // TODO: PULL QUESTIONS FROM DB ?
   useEffect(() => {
-    setQuestions([]);
-    setInitialQuestions([]);
+    const getQuiz = async () => {
+      if (quizId) {
+        const quizResponse = await client.findQuizById(quizId);
+        setQuiz(quizResponse);
+        setInitialQuestions(quizResponse.questions);
+        setQuestions(quizResponse.questions);
+      }
+    };
+    getQuiz();
   }, []);
 
   const addNewQuestion = () => {
     const newQuestion: Question = {
       title: "",
-      type: QuestionType.MultipleChoice,
+      type: QuestionType.MULTIPLE_CHOICE,
       points: 0,
       description: "",
       answers: [],
@@ -41,10 +51,16 @@ function QuestionsTab() {
     setQuestions(updatedQuestions);
   };
 
-  const handleSave = () => {
-    // TODO: MAKE BACKEND REQUESTS
-    // post all newQuestions
-    // put all questions
+  const handleSave = async (publish?: boolean) => {
+    if (quiz) {
+      const points = questions.reduce((acc, curr) => acc + curr.points, 0);
+      let newQuiz = { ...quiz, questions, points };
+      if (publish) {
+        newQuiz = { ...newQuiz, isPublished: true };
+      }
+      await client.updateQuiz(newQuiz);
+      setQuiz(newQuiz);
+    }
   };
 
   const handleCancel = () => {
@@ -52,16 +68,15 @@ function QuestionsTab() {
   };
 
   const handleSaveAndPublish = () => {
-    handleSave();
-    // TODO: ADD PUBLISH QUIZ
+    handleSave(true);
   };
 
   return (
     <div className="flex-fill">
       <div className="question-editor-header">
-        <div className="points-text">Points {points} </div>
+        <div className="points-text">Points {quiz ? quiz.points : "0"} </div>
         <div>
-          {published ? (
+          {quiz && quiz.isPublished ? (
             <>
               <FaCheck style={{ alignSelf: "center", color: "green" }} />{" "}
               Published
@@ -75,6 +90,7 @@ function QuestionsTab() {
         </div>
       </div>
       <hr />
+      <EditorNav />
       <div className="questions">
         {questions.map((question, idx) => (
           <QuestionEditor
@@ -108,12 +124,8 @@ function QuestionsTab() {
       <hr />
       <div className="question-editor-publish-footer">
         <label>
-          <input
-            type="checkbox"
-            name="notify"
-            onChange={(e) => setNotifyUsers(e.target.checked)}
-          />{" "}
-          Notify users this quiz has changed
+          <input type="checkbox" name="notify" /> Notify users this quiz has
+          changed
         </label>
         <div className="d-flex flex-row">
           <button className="btn wd-modules-btn" onClick={handleCancel}>
@@ -122,7 +134,7 @@ function QuestionsTab() {
           <button className="btn wd-modules-btn" onClick={handleSaveAndPublish}>
             Save & Publish
           </button>
-          <button className="btn btn-danger" onClick={handleSave}>
+          <button className="btn btn-danger" onClick={() => handleSave()}>
             Save
           </button>
         </div>
